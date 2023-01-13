cb069f11d720cc355e183ddb24230e6b21dcdd81
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 15:50:10 +0530

    Creating a Simple LinkedList

---
+++ LinkedList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day14LinkedList
+{
+    public class LinkedList
+    {
+        private int size;
+        private Node head;
+
+        public LinkedList()
+        {
+            this.size = 0;
+            this.head = null;
+        }
+
+        /**
+         * Inserting new node at the end of the linked list
+         * 
+         * @param data - represent the node data to be added to the linked list
+         */
+        public void insertNode(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            Node current = this.head;
+
+            if (this.head == null)
+            {
+                this.head = node;
+                this.head.next = null;
+                this.size = 1;
+                Console.WriteLine(this.head);
+            }
+            else
+            {
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = node;
+                node.next = null;
+                this.size += 1;
+            }
+        }
+
+
+        public void printNodes()
+        {
+            if (this.size < 1)
+                Console.WriteLine("There are no nodes in the linked list");
+
+            else
+            {
+                Node current = this.head;
+                for (int i = 0; i < this.size; i++)
+                {
+                    Console.WriteLine("Node " + current.data + " is at location " + i);
+                    current = current.next;
+                }
+            }
+        }
+
+
+        public int getListSize()
+        {
+            return size;
+        }
+    }
+}
