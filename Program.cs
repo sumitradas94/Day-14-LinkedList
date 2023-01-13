072880f6f0e098c4ac451e92b22cf5efde6e3b4e
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 15:47:41 +0530

    modified

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Day14LinkedList;
+
+namespace day14LinkedLst
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(70);
+            list.printNodes();
+        }
+    }
+}
