ec2c8013fbae778d15d50fcbb433a3ca07e9c9cd
sumitradas94 <sumitradas94@gmail.com>
2023-01-13 16:00:58 +0530

    modified for UC2

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ namespace day14LinkedLst
     {
         public static void Main(String[] args)
         {
-            LinkedList list = new LinkedList();
+            // LinkedList list = new LinkedList();
 
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            //Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
-            list.insertNode(56);
+            // list.insertNode(56);
+            //list.insertNode(30);
+            // list.insertNode(70);
+            //list.printNodes();
+
+            AddingLinkedList list = new AddingLinkedList();
             list.insertNode(30);
             list.insertNode(70);
+            list.insertNode(56);
             list.printNodes();
         }
     }
