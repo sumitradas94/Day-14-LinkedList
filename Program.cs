e645adf20afa23a9932ece1ac87560531423b651
sumitradas94 <sumitradas94@gmail.com>
2023-01-14 07:54:06 +0530

    Ordered Linked List in Ascending Order

--- Program.cs
+++ Program.cs
@@ -32,15 +32,31 @@ namespace LinkedList
             list.findNode(30);
             list.printNodes();*/
 
-            insert40 list = new insert40();
+            /*insert40 list = new insert40();
+             list.insertNode(56);
+             list.insertNode(30);
+             list.insertNode(70);
+             list.printNodes();
+             Console.WriteLine("---------------");
+             Console.WriteLine("Adding 40 at second Location");
+             list.insertNth(40, 2);
+             list.printNodes();*/
+
+            Delete40 list = new Delete40();
+            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+
             list.insertNode(56);
             list.insertNode(30);
+            list.insertNode(40);
             list.insertNode(70);
             list.printNodes();
-            Console.WriteLine("---------------");
-            Console.WriteLine("Adding 40 at second Location");
-            list.insertNth(40, 2);
+            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+
+            Console.WriteLine("-------------");
+            Console.WriteLine("Deleting node from 2nd location");
+            list.deleteNthNode(2);
             list.printNodes();
+            Console.WriteLine("Current Size of the list is: " + list.getListSize());
         }
     }
 }
