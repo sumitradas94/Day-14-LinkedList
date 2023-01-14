811b475d30c7d1715b10673b72ed1f3691c0fd17
sumitradas94 <sumitradas94@gmail.com>
2023-01-14 07:55:01 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -7,56 +7,18 @@ namespace LinkedList
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome to Linked List Problem");
-            /* DeleteLast list = new DeleteLast();
-             Console.WriteLine("Current Size of the list is: " + list.getListSize());
-
-             list.insertNode(56);
-             list.insertNode(30);
-             list.insertNode(70);
-             list.printNodes();
-
-             Console.WriteLine("---------------");
-             Console.WriteLine("Deleting Last node");
-             list.deleteLastNode();
-             list.printNodes();*/
-
-            /*SearchLS list = new SearchLS();
-            list.insertNode(56);
-            list.insertNode(30);
-            list.insertNode(70);
-            list.printNodes();
-
-            Console.WriteLine("----------------");
-            Console.WriteLine("Finding 30 in the Lineked list");
-            list.findNode(30);
-            list.printNodes();*/
-
-            /*insert40 list = new insert40();
-             list.insertNode(56);
-             list.insertNode(30);
-             list.insertNode(70);
-             list.printNodes();
-             Console.WriteLine("---------------");
-             Console.WriteLine("Adding 40 at second Location");
-             list.insertNth(40, 2);
-             list.printNodes();*/
-
-            Delete40 list = new Delete40();
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            Console.WriteLine("Welcome to Linked List Problem");
+            OrderdLinkedList list = new OrderdLinkedList();
 
             list.insertNode(56);
             list.insertNode(30);
             list.insertNode(40);
             list.insertNode(70);
             list.printNodes();
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
-            Console.WriteLine("-------------");
-            Console.WriteLine("Deleting node from 2nd location");
-            list.deleteNthNode(2);
-            list.printNodes();
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            Console.WriteLine();
+            list.orderNodes();
+
         }
     }
 }
